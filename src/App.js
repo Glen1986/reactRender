d1e48957c328a028defbfa4428fc25c48f1259c2
Glen1986 <escobar.glen@gmail.com>
2022-01-19 13:30:37 -0300

    renderForm4

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { useState } from "react";
 export default function App(props) {
   const [notes, setNotes]= useState(props.notes)
   const [newNote, setNewNote]= useState('') 
+  const [showAll, setShowAll] = useState(true)
 
   const handleEvent =(e)=>{
     setNewNote(e.target.value)
 }
-const handleClick = ()=> {
+const handleSubmit = (e)=> {
+  e.preventDefault();
 
   const noteToAddToState = {
     id : notes.length + 1,
@@ -19,15 +21,25 @@ const handleClick = ()=> {
 setNotes([...notes, noteToAddToState])
   setNewNote('')
 }
+const handleShowAll = () =>{
+  setShowAll(()=> !showAll)
+}
   return (
     <>
+      <h1>Notas</h1>
+      <button onClick={handleShowAll}>{showAll ? 'todos' : 'solo importantes'}</button>
     <ol>
-      {notes.map((note) => <Note  key={note.id} {...note}/> )}   
+      {notes
+          .filter(note =>{
+            if(showAll === true) return note
+            return note.important === true
+      })
+      .map((note) => <Note  key={note.id} {...note}/> )}   
     </ol>
-      <div>
+      <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleEvent} value={newNote}/>
-        <button onClick={handleClick}>crear nota</button>
-      </div>
+        <button >crear nota</button>
+      </form>
     </>
   );
 }
