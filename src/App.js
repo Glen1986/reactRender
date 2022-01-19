41a7e1e9b839facd47a7440e583fe0793f63e043
Glen1986 <escobar.glen@gmail.com>
2022-01-19 12:58:03 -0300

    renderForm3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import { Note } from "./Note";
-import Form from './components/Form'
+import { useState } from "react";
 
-export default function App({notes=[]}) {
+export default function App(props) {
+  const [notes, setNotes]= useState(props.notes)
+  const [newNote, setNewNote]= useState('') 
+
+  const handleEvent =(e)=>{
+    setNewNote(e.target.value)
+}
+const handleClick = ()=> {
+
+  const noteToAddToState = {
+    id : notes.length + 1,
+    content: newNote,
+    date: new Date().toISOString(),
+    important:Math.random(),
+  }
+setNotes([...notes, noteToAddToState])
+  setNewNote('')
+}
   return (
     <>
     <ol>
       {notes.map((note) => <Note  key={note.id} {...note}/> )}   
     </ol>
-      <Form></Form>
+      <div>
+        <input type="text" onChange={handleEvent} value={newNote}/>
+        <button onClick={handleClick}>crear nota</button>
+      </div>
     </>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,28 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
-export default function Form() {
+export default function Form(props) {
+    const [notes, setNotes]= useState(props.notes)
+    const [newNote, setNewNote]= useState('') 
+
+    const handleEvent =(e)=>{
+        setNewNote(e.target.value)
+        
+}
+const handleClick = ()=> {
+  const noteToAddToState = {
+      id : notes.length + 1,
+          content: newNote,
+              date: new Date().toISOString(),
+        important:Math.random(),
+      
+}
+  console.log(noteToAddToState)
+setNotes(notes.concat(noteToAddToState))
+
+}
   return (
     <Box
       component="form"
@@ -10,9 +30,10 @@ export default function Form() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined"onChange={handleEvent} value={content}/>
+      <TextField id="filled-basic" label="Filled" variant="filled"value={date} onChange={handleEvent} />
+      <TextField id="standard-basic" label="Standard" variant="standard" value={important}onChange={handleEvent} />
+      <button onClick={handleClick}></button>
     </Box>
   );
 }
